94bb35930ad5741507f57d4ae8e72bb31d2de5ad
marcodanielcameros <marcocameros@hotmail.com>
2016-11-06 18:16:13 -0600

    Add files via upload
    Clase 2

--- arbol.pl
+++ arbol.pl
@@ -5,6 +5,13 @@ madre(elisa,sara).
 madre(elisa,beto).
 madre(elisa,gerardo).
 
+padre(sabino,lucina).
+padre(sabino,gonzalo).
+padre(sabino,triunfo).
+padre(sabino,sara).
+padre(sabino,beto).
+padre(sabino,gerardo).
+
 madre(sara,javier).
 madre(sara,marco).
 madre(sara,diego).
@@ -14,9 +21,21 @@ padre(gerardo,luis).
 padre(gerardo,carlos).
 padre(gerardo,javier).
 padre(gerardo,monica).
-padre(gerardo,berardo).
+padre(gerardo,bernardo).
+
+madre(maria,luis).
+madre(maria,carlos).
+madre(maria,javier).
+madre(maria,monica).
+madre(maria,bernardo).
 
 padre(javier,francisco).
 padre(javier,marco).
 padre(javier,diego).
 padre(javier,ilse).
+
+abuelo(X,Y):- padre(X,Z),padre(Z,Y).
+abuelo(X,Y):- padre(X,Z),madre(Z,Y).
+
+hermano(X,Y):- padre(Z,X),padre(Z,Y).
+tio(X,Y):- padre(Z,Y),hermano(Z,X).
