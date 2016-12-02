e4246ab77d44b4ee354e3350545711dff1b14dca
marcodanielcameros <marcocameros@hotmail.com>
2016-12-02 08:00:52 -0600

    Create Factorial.go

---
+++ Factorial.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+	
+	return x * factorial(x-1)
+}
+
+func main() {
+	x := uint(6)
+	calcFactorial := factorial(x)
+	fmt.Println(calcFactorial)
+}
